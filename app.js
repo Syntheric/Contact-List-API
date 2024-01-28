fd3a1414bc51f55a5137d148fba312597312abc8
Syntheric <MrSyntheric@gmail.com>
2024-01-28 18:17:12 +0700

    Created and polished => [CRUD]

--- app.js
+++ app.js
@@ -32,19 +32,86 @@ app.post('/api/contact-create', (req,res) => {
     })
 })
 
-/* Update Data */
+/* Update Data*/
 
 app.put('/api/contact-update/:id', (req,res) => {
     const data = {...req.body}
     const queryFindById = 'SELECT * FROM contacts WHERE id = ?' 
-    const queryUpdate = 'UPDATE  contacts SET ? WHERE id = ?'
-    
+    const queryUpdate = 'UPDATE contacts SET ? WHERE id = ?'
+
     /*Query Find Data */
     db.query(queryFindById, req.params.id, (err, rows, field) => {
+
+        /*Error Handling*/
         if (err) {
             return res.status(500),json({status_code : 500,msg : 'Failed find data', err: err})
         }
-        console.log(rows);
-        // res.status(200).json({status_code : 200,success : true ,msg : "Successfully updated "})
+        if (rows.length) {
+
+            const types = ['ID', 'SGP', 'MLY', 'TH', 'PH', 'VIET', 'MY', 'LS']
+
+            if (!types.includes(data.type)) {
+                return res.status(500).json({status_code : 500,msg : 'Type Not Found'})
+            }
+            /*Query Update Data*/
+            
+            db.query(queryUpdate, [data, req.params.id] , (err, rows, field) => {
+                if (err) {
+                    return res.status(500).json({status_code: 500, msg: 'Failed to update data', err: err});
+                }
+                res.status(200).json({status_code:200, success: true , msg: 'Success updating data', row: rows});
+            })
+        }else{
+            return res.status(404).json({status_code: 404, msg : `Error updating data ${req.params.id} not found`})
+        }
+    })
+})
+
+
+/* Delete Data */
+
+app.delete('/api/contact-delete', (req,res) => {
+
+    const data = {...req.body}
+    const queryFindById = 'SELECT * FROM contacts WHERE id = ?' 
+    const queryDelete = 'DELETE FROM contacts WHERE id = ?'
+
+    /*Query Find Data */
+    db.query(queryFindById, req.body, (err, rows, field) => {
+
+        /*Error Handling*/
+        if (err) {
+            return res.status(500),json({status_code : 500,msg : 'Failed find data', err: err})
+        }
+        if (rows.length) {
+            /*Query Delete Data*/
+            
+            db.query(queryDelete, [data, req.body] , (err, rows, field) => {
+                if (err) {
+                    return res.status(500).json({status_code: 500, msg: 'Failed to delete data', err: err});
+                }
+                res.status(200).json({status_code:200, success: true , msg: 'Success delete data', row: rows});
+            })
+        }else{
+            return res.status(404).json({status_code: 404, msg : `Error delete data ${req.body} not found`})
+        }
+    })
+})
+
+/*Get Data*/
+
+app.get('/api/contact-get/:id', (req,res) => {
+
+    // const data = {...req.body}
+
+    const queryFindById = 'SELECT * FROM contacts WHERE id = ?' 
+
+    /*Query Find Data */
+
+    db.query(queryFindById, req.params.id , (err, rows, field) => {
+        if (err) {
+            return res.status(500).json({status_code: 500, msg: 'Failed to read data', err: err});
+        }
+        res.status(200).json({status_code:200, success: true , msg: 'Success reading data', row: rows});
     })
 })
